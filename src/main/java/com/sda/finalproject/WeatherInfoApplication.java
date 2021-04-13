f7f63ca33d030a6e92058c21253e51cd61fa8ed3
BiancaHerghelegiu <bianca.herghelegiu98@gmail.com>
2021-04-13 19:43:20 +0300

    UP_3 added main class @Ana P

---
+++ src/main/java/com/sda/finalproject/WeatherInfoApplication.java
@@ -0,0 +1,12 @@
+package com.sda.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherInfoApplication.class);
+    }
+}
